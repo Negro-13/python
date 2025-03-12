e4c0b140993e5741dadbe9cdaca821067db07fe8
Negro_13 <141974651+Negro-13@users.noreply.github.com>
2025-03-11 22:49:56 -0300

    Update ej8.py

--- Guia de phyton1/ej8.py
+++ Guia de phyton1/ej8.py
@@ -1,2 +1,14 @@
 print('Ingrese su numero')
 num = int(input())
+
+if num == 0 :
+    print('Su numero es cero')
+if num < 0:
+    print('Su numero es negativo')
+else:
+    print('Su numero es positivo')
+
+if num % 2 == 0:
+    print('Su numero es par')
+else:
+    print('Su numero es impar')
